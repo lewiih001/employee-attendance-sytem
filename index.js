fb68e4c92b11f48fe05ec26f4cd12dcdba25b453
Lewis <lewismunyambu@gmail.com>
2022-08-31 22:45:22 +0300

    changed index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let text = '{ "employees" : [' +
-'{ "firstName":"John" , "lastName":"Doe" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" },' +
-'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+let element = '{ "employees" : [' +
+'{ "firstName":"Lewis" , "lastName":"Ndila" },' +
+'{ "firstName":"Kelvin" , "lastName":"Kweyu" },' +
+'{ "firstName":"Susan" , "lastName":"Rose" } ]}';
 
 document.getElementById("demo").innerHTML =
 obj.employees[1].firstName + " " + obj.employees[1].lastName;
@@ -9,3 +9,14 @@ obj.employees[1].firstName + " " + obj.employees[1].lastName;
 element.addEventListener("click", function() {
     document.getElementById("demo").innerHTML = " ";
   });
+
+  const myObj = {name: "Lewis", age: 21, };
+  const myJSON = JSON.stringify(myObj);
+  localStorage.setItem("testJSON", myJSON);
+  
+  
+  const text = localStorage.getItem("testJSON");
+  let obj = JSON.parse(text);
+  document.getElementById("demo").innerHTML = obj.name;
+  document.getElementById("demo").innerHTML =
+obj.employees[1].firstName + " " + obj.employees[1].lastName;
